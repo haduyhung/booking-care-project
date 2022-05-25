9161a9b901ef812894da295182b13140f212b7ce
haduyhung <haduyhungdz123@gmail.com>
2022-05-25 11:22:48 +0700

    add header

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,3 @@
+import Logo from "./images/Logo.svg";
+
+export { Logo };
--- frontend/src/layout/Header/index.js
+++ frontend/src/layout/Header/index.js
@@ -1,22 +1,87 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
-import { Container, IconButton, Toolbar } from "@mui/material";
+import { Box, Container, IconButton, Link, Toolbar } from "@mui/material";
+import * as images from "../../assets/index";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { BsQuestionCircleFill } from "react-icons/bs";
+
+const pages = [
+  { name: "Chuyên khoa", explain: "Tìm bác sĩ chuyên khoa", url: "/" },
+  { name: "Cơ sở y tế", explain: "Chọn bệnh viện phòng khám", url: "/" },
+  { name: "Bác sĩ", explain: "Chọn bác sĩ giỏi", url: "/" },
+  { name: "Gói khám", explain: "Khám sức khoẻ tổng quát", url: "/" },
+];
 
 const Header = () => {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
+    <AppBar position="static" color="inherit">
+      <Container maxWidth="lg">
         <Toolbar disableGutters>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
+          <Box sx={{ flexGrow: 1, display: "flex" }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <GiHamburgerMenu color="#888888" />
+              </IconButton>
+              <Link href="/">
+                <img src={images.Logo} alt="Booking Care" width={160} />
+              </Link>
+            </Box>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+              }}
+              py={3}
+            >
+              {pages.map((page) => (
+                <Box key={page.name} mx={2.5}>
+                  <Link
+                    href={page.url}
+                    underline="none"
+                    color="black"
+                    fontWeight="700"
+                    fontSize={13}
+                  >
+                    {page.name}
+                  </Link>
+                  <Box>
+                    <Link
+                      href={page.url}
+                      underline="none"
+                      color="black"
+                      fontWeight="400"
+                      fontSize={11}
+                    >
+                      {page.explain}
+                    </Link>
+                  </Box>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+
+          <Link
+            textAlign="right"
+            href="/"
+            underline="none"
+            color="#999999"
+            fontSize={14}
+            fontWeight="600"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              flexGrow: 0,
+            }}
           >
-            <GiHamburgerMenu />
-          </IconButton>
+            <BsQuestionCircleFill color="#45C8DF" />
+            Hỗ trợ
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
