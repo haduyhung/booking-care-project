b5c7202d52e86737b5ed52a8b94aeb7fce28a579
DungDMRP <dungdm@reactplus.jp>
2022-07-05 22:29:02 +0700

    create_admin

--- frontend/src/apis/UserApi/index.js
+++ frontend/src/apis/UserApi/index.js
@@ -1,32 +1,32 @@
-import requestClient from "./requestClient"
+import requestClient from "../RequestClient";
 
 const UserApi = {
-	getAll(token) {
-		const urlParam = "users"
+  getAll(token) {
+    const urlParam = "user";
 
-		return requestClient.get(urlParam)
-	},
-	getOne(id, token) {
-		const urlParam = `users/${id}`
+    return requestClient.get(urlParam, token);
+  },
+  getOne(id, token) {
+    const urlParam = `user/${id}`;
 
-		return requestClient.get(urlParam)
-	},
+    return requestClient.get(urlParam, token);
+  },
 
-	deleteUser(id, user) {
-		const urlParam = `users/${id}`
+  deleteUser(id, token) {
+    const urlParam = `user/${id}`;
 
-		return requestClient.delete(urlParam)
-	},
+    return requestClient.delete(urlParam, token);
+  },
 
-	update(id, data, user) {
-		const urlParam = `users/${id}`
+  update(id, data, token) {
+    const urlParam = `user/${id}`;
 
-		return requestClient.put(urlParam, data)
-	},
-	registerOut(data) {
-		const urlParam = "users/register/out"
-		return requestClient.post(urlParam, data)
-	},
-}
+    return requestClient.put(urlParam, data, token);
+  },
+  registerOut(data) {
+    const urlParam = "users/register/out";
+    return requestClient.post(urlParam, data);
+  },
+};
 
-export default UserApi
+export default UserApi;
